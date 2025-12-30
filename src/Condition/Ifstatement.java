007018f09dba7060834b2d072120fc1d3c1e2de9
pujak3432-coder <pujak3432@hmail.com>
2025-12-30 19:01:42 +0530

    commit

---
+++ src/Condition/Ifstatement.java
@@ -0,0 +1,14 @@
+package Condition;
+
+public class Ifstatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=10;
+		if (num>0) {
+			System.out.println("positive no.");
+		}
+
+	}
+
+}
